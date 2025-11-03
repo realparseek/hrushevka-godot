e1b0d26011540e19d6da32b5b1d5de103293be6a
ANS-svg <ggkotgg01@gmail.com>
2025-11-04 01:57:48 +0500

    Audio engine

--- scripts/radio.gd
+++ scripts/radio.gd
@@ -1,7 +1,6 @@
-extends AudioStreamPlayer3D
+extends SteamAudioPlayer
 
-# Массив с аудиофайлами
-var tracks = [
+@export var tracks = [
 	preload("res://sounds/radio/commuter_train.ogg"),
 	preload("res://sounds/radio/Cuckoo.ogg"),
 	preload("res://sounds/radio/Summer_will_be_over.ogg")
@@ -9,19 +8,23 @@ var tracks = [
 
 var current_track_index = 0
 var radio_playing = false
+var time_out_of_zone = 0.0
+var timer_started = false
 
 func _ready():
 	select_random_track()
-	
 	self.finished.connect(_on_track_finished)
-	play_current_track()
+	self.stream_paused = true
+
+func _process(delta):
+	if timer_started:
+		time_out_of_zone += delta
 
 func select_random_track():
 	if tracks.size() > 0:
 		current_track_index = randi() % tracks.size()
 
 func _on_track_finished():
-	# Автоматическое переключение на следующий трек
 	current_track_index = (current_track_index + 1) % tracks.size()
 	play_current_track()
 
@@ -30,27 +33,20 @@ func play_current_track():
 		self.stream = tracks[current_track_index]
 		self.play()
 		radio_playing = true
-		print("Now playing track: ", current_track_index + 1)  # Для отладки
 
-func play_next_track():
-	current_track_index = (current_track_index + 1) % tracks.size()
-	play_current_track()
-
-func play_previous_track():
-	current_track_index = (current_track_index - 1) % tracks.size()
-	if current_track_index < 0:
-		current_track_index = tracks.size() - 1
-	play_current_track()
-
-# Функция для принудительного случайного выбора
-func play_random_track():
-	select_random_track()
-	play_current_track()
-
-func toggle_play_pause():
-	if radio_playing:
-		self.stop()
-		radio_playing = false
-	else:
-		self.play()
+func start_radio():
+	if tracks.size() > 0:
+		if time_out_of_zone > 60.0:
+			select_random_track()
+			play_current_track()
+		if not radio_playing:
+			self.stream = tracks[current_track_index]
+			self.play()
 		radio_playing = true
+		self.stream_paused = false
+		timer_started = false
+		time_out_of_zone = 0.0
+
+func stop_radio():
+	self.stream_paused = true
+	timer_started = true
